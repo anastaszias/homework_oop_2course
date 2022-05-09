f866b099cb7c90557001105394cc29a4104d4ece
anastaszias <103033198+anastaszias@users.noreply.github.com>
2022-05-09 11:36:15 +0300

    Add files via upload

---
+++ DZ9/DZ9.cpp
@@ -0,0 +1,12 @@
+﻿#include "fun9.h"
+int main() {
+	setlocale(0, "");
+	Property* massive[7] = { new Apartment(0), new Apartment(0), new Apartment(0),
+							 new Car(0), new Car(0), new CountryHouse(0),new CountryHouse(0) };
+	fill_the_cost_of_property_random(massive, 7);
+	cout << endl;
+	for (int i = 0; i < 7; i++) {
+		cout << massive[i]->tax() << endl;
+		delete massive[i];
+	}
+}
