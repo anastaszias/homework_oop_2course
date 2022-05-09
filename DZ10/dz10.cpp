b9ff22c21dbd77639b8343520872d4d45b496bcb
anastaszias <103033198+anastaszias@users.noreply.github.com>
2022-05-09 16:22:21 +0300

    Add files via upload

---
+++ DZ10/dz10.cpp
@@ -0,0 +1,10 @@
+﻿#include "fun10.h"
+int main() {
+	setlocale(0, "");
+	Human human("Хабарова", "Лариса", "Валериевна", 50);
+	Student student("Хабарова", "Анастасия", "Вадимовна", 19, 1);
+	Boss boss("Хабаров", "Вадим", "Борисович", 50, 200);
+	human.print();
+	student.print();
+	boss.print();
+}
---
+++ DZ10/fun10.cpp
@@ -0,0 +1,13 @@
+﻿#include "fun10.h"	
+void Student::print() {
+	Human(this->surname, this->name, this->midname, this->age).print();
+	cout << ", Íàõîäèòñÿ";
+	if (on_lesson)
+		cout << " íà óðîêå\n";
+	else
+		cout << " íå íà óðîêå\n";
+}
+void Boss::print() {
+	Human(this->surname, this->name, this->midname, this->age).print();
+	cout << ", Êîëè÷åñòâî ðàáîòíèêîâ â ïîä÷èíåíèè: " << number_of_workers << endl;
+}
