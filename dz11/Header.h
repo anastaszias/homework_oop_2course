96e091a60e80b788dbed4c2e5867e1870ce4cab0
anastaszias <103033198+anastaszias@users.noreply.github.com>
2022-06-15 09:00:42 +0300

    Add files via upload

---
+++ dz11/Header.h
@@ -0,0 +1,44 @@
+﻿#ifndef HEADER_H
+#define HEADER_H
+using namespace std;
+#include<iostream>
+class screen {
+protected:
+	bool built_in_screen, HDMI;//ýêðàí âñòðîåí èëè íåò
+public:
+	screen(bool a = false, bool b = false) :built_in_screen(a), HDMI(b) {};
+	virtual void print();
+};
+class keyboard {
+protected:
+	bool built_in_keyboard, USB, digital;//êëàâèàòóðà âòðîåíà èëè íåò (èëè âîîáùå âèðòóàëüíàÿ)
+public:
+	keyboard(bool a = false, bool b = false, bool c = false) : built_in_keyboard(a), USB(b), digital(c) {};
+	virtual void print();
+};
+class notebook :public keyboard, screen {
+public:
+	notebook(bool b = false, bool e = false, bool d = false) :keyboard(1, b, 1), screen(1, d) {};//â íîóòáóêå òî÷íî åñòü âñòðîåííûå êëàâèàòóðà è ìîíèòîðû, à òàêæå âèðóòàëüíàÿ êëàâèàòóðà
+	void print();
+	void fill_USB(bool n) { this->USB = n; }
+	void fill_HDMI(bool n) { this->HDMI = n; }
+};
+class personal_computeer :public keyboard, screen {
+public:
+	personal_computeer(bool b = false, bool c = false, bool d = false) :keyboard(0, b, 1), screen(0, d) {};//â ïåðñîíàëüíîì êîìïþòåðå íåò âñòðîåííûõ êëàâèàòóðû è ìîíèòîðà,íî ïðèñóòñâóòå âèðòóàëüíàÿ êëàâèàòóðà, à äîïîëíèòåëüíûå ýêðàíû è ìîíèòîðû -îïöèîíàëüíî
+	void print();
+	void fill_USB(bool n) { this->USB = n; }
+	void fill_HDMI(bool n) { this->HDMI = n; }
+};
+class phone :public keyboard, screen {
+public:
+	phone(bool a = false, bool b = false, bool d = false) :keyboard(a, b, 1), screen(1, d) {};//ê òåëåôîíó ìîæíî âñå ïîäêëþ÷èòü ÷åðåç ïåðåõîäíèêè, à òàêæå ïðèñóòñòâóþò âñòðîåííûå êëàâèàòóðà è ìîíèòîð
+	void print();
+	void fill_built_in_keyboard(bool n) { this->built_in_keyboard = n; }
+	void fill_USB(bool n) { this->USB = n; }
+	void fill_HDMI(bool n) { this->HDMI = n; }
+};
+void fill_rand(notebook a);
+void fill_rand(personal_computeer a);
+void fill_rand(phone a);
+#endif #pragma once
---
+++ dz11/Source.cpp
@@ -0,0 +1,75 @@
+﻿#include "head.h"
+void screen::print()
+{
+	if (this->built_in_screen) {
+		cout << "\nÂñòðîåííûé ýêðàí ïîäêëþ÷åí";
+	}
+	else {
+		cout << "\nÂñòðîåííûé ýêðàí íå ïîäêëþ÷åí";
+	}
+	if (this->HDMI) {
+		cout << "\nÝêðàí ïîäêëþ÷åí ÷åðåç ïîðò HDMI";
+	}
+	else {
+		cout << "\nÝêðàí íå ïîäêëþ÷åí ÷åðåç ïîðò HDMI";
+	}
+}
+void keyboard::print()
+{
+	if (this->built_in_keyboard) {
+		cout << "\nÂñòðîåííàÿ êëàâèàòóðà ïîäêëþ÷åíà";
+	}
+	else {
+		cout << "\nÂñòðîåííàÿ êëàâèàòóðà íå ïîäêëþ÷åíà";
+	}
+	if (this->USB) {
+		cout << "\nUSB-êëàâèàòóðà ïîäêëþ÷åíà";
+	}
+	else {
+		cout << "\nUSB-êëàâèàòóðà íå ïîäêëþ÷åíà";
+	}
+	if (this->digital) {
+		cout << "\nÂèðòóàëüíàÿ êëàâèàòóðà ïîäêëþ÷åíà";
+	}
+	else {
+		cout << "\nÂèðòóàëüíàÿ êëàâèàòóðà íå ïîäêëþ÷åíà";
+	}
+}
+void notebook::print() {
+	cout << "\n Íîóòáóê:\n";
+	keyboard(this->built_in_keyboard, this->digital, this->USB).print();
+	screen(this->built_in_screen, this->HDMI).print();
+	cout << endl;
+}
+void personal_computeer::print() {
+	cout << "\n Ïåðñîíàëüíûé êîìïüþòåð\n";
+	keyboard(this->built_in_keyboard, this->digital, this->USB).print();
+	screen(this->built_in_screen, this->HDMI).print();
+	cout << endl;
+}
+void phone::print() {
+	cout << "\n Òåëåôîí\n";
+	keyboard(this->built_in_keyboard, this->digital, this->USB).print();
+	screen(this->built_in_screen, this->HDMI).print();
+	cout << endl;
+}
+void fill_rand(notebook a) {
+	srand(time(NULL));
+	int z = rand() % 2, x = rand() % 2;
+	a.fill_USB(z);
+	a.fill_HDMI(x);
+}
+void fill_rand(personal_computeer a)
+{
+	srand(time(NULL));
+	int z = rand() % 2, x = rand() % 2;
+	a.fill_USB(z);
+	a.fill_HDMI(x);
+}
+void fill_rand(phone a)
+{
+	int z = rand() % 2, x = rand() % 2, c = rand() % 2;
+	a.fill_built_in_keyboard(z);
+	a.fill_USB(x);
+	a.fill_HDMI(c);
+}
